b0b086714d1b2985c6cc2a807760074fcc367983
Sakyi-Ken <esappiah009@st.ug.edu.gh>
2023-12-18 09:30:35 +0000

    updated the patient file

--- patient.js
+++ patient.js
@@ -1,9 +1,10 @@
+// patients.js
+
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
-
 const Patient = require('../models/Patient');
 
+// Route to register a new patient
 router.post('/', async (req, res) => {
   try {
     const {
@@ -35,6 +36,29 @@ router.post('/', async (req, res) => {
     await newPatient.save();
 
     res.status(201).json({ message: 'Patient registered successfully', patient: newPatient });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to get a list of all patients
+router.get('/', async (req, res) => {
+  try {
+    const patients = await Patient.find();
+    res.status(200).json(patients);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Route to get details of a specific patient by ID
+router.get('/:patientId', async (req, res) => {
+  try {
+    const patient = await Patient.findById(req.params.patientId);
+    if (!patient) {
+      return res.status(404).json({ message: 'Patient not found' });
+    }
+    res.status(200).json(patient);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
